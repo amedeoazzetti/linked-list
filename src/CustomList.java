c7c24c8f7990c08de9e68dc3cb6349b5b5dcc620
Amedeo Azzetti <amedeo.azzetti@marconirovereto.it>
2026-01-08 12:02:39 +0100

    fatto metodo head e pop finire metodo push

--- src/CustomList.java
+++ src/CustomList.java
@@ -1,4 +1,3 @@
-
 public class CustomList {
 
     // startin node of the structure
---
+++ src/Stack.java
@@ -0,0 +1,51 @@
+public class Stack {
+    // head of the list
+    private Node top;
+
+    public Stack(){
+        this.top = null;
+    }
+
+    public Stack(Node newNode){
+        this.top = newNode;
+    }
+
+    public boolean isEmpty(){
+
+        return top == null;
+
+    }
+
+    /**
+     *  Legge il primo elemento 
+     * @return contenuto del primo nodo
+     */
+    public int head(){
+        return top.getValue();
+    }
+    
+
+    /**
+     * Rimuove il primo elemento dallo stack, aggiornando il riferimento
+     * @return nodo rimosso
+     */
+    public Node pop(){
+        Node cursor = top;
+        Node rimosso = top.getNext();
+        cursor.getNext().getNext();
+        return rimosso;
+        
+    }
+
+    /**
+     * Aggiunge un nuovo nodo sopra allo stack
+     * @param nowNode nodo aggiunto
+     */
+    public void push(Node nowNode){
+        Node cursor = top;
+        
+        cursor.setNext(nowNode);
+        
+    }
+    
+}
